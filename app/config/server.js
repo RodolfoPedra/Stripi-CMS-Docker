e8190664e661ec92b978b74fc2b31e73f3e53187
RodolfoPedra <rodolfoaldrovandip@gmail.com>
2022-02-24 11:42:22 -0300

    primeiro commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '18bde768556735983c48acc786a48b77'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c86847e3-53dc-47e9-985e-4c197bcd6cb9'
+};
